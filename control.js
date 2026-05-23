dfb74006f9d74dc645af6e4934c3854edaf156c3
Njabulo Jb <fanajb65@gmail.com>
2026-05-23 04:34:16 +0200

    Update control.js

--- control.js
+++ control.js
@@ -875,6 +875,16 @@ setTimeout(() => {
                 }
             });
         };
+
+        
+const { handleButtons } = require("./commands/play0");
+
+zk.ev.on("messages.upsert", async (m) => {
+  const msg = m.messages[0];
+  if (!msg.message) return;
+
+  await handleButtons(zk, msg);
+});
         
         return zk;
     }
