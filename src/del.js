79fd8b2df30ccc8c45c6f5f2ab45daee51971168
Njabulo Jb <fanajb65@gmail.com>
2026-05-24 13:27:39 +0200

    Update del.js

--- src/del.js
+++ src/del.js
@@ -32,11 +32,13 @@ const buttons = [
 
 // ---------- Random image ----------
 const njabulox = [
-  "https://files.catbox.moe/iii5jv.jpg",
-  "https://files.catbox.moe/xjeyjh.jpg",
-  "https://files.catbox.moe/mh36c7.jpg",
-  "https://files.catbox.moe/u6v5ir.jpg",
-  "https://files.catbox.moe/bnb3vx.jpg",
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg.png",
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg2.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg3.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg4.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg5.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg.png"
+
 ];
 const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
 
@@ -50,33 +52,7 @@ async function sendFormattedMessage(zk, chatId, text, ms) {
         header: text,
         buttons: buttons,
         headerType: 1,
-        contextInfo: {
-          mentionedJid: [ms?.sender?.jid || ""],
-          externalAdReply: {
-            title: "📝messages menu cmd",
-            mediaType: 1,
-            previewType: 0,
-            thumbnailUrl: randomNjabulourl,
-            sourceUrl: "https://www.instagram.com/njabulojb871",
-            renderLargerThumbnail: false,
-          },
-        },
-      },
-    },
-    {
-      quoted: {
-        key: {
-          fromMe: false,
-          participant: "0@s.whatsapp.net",
-          remoteJid: "status@broadcast",
-        },
-        message: {
-          contactMessage: {
-            displayName: "🟢online njᥲbᥙᥣo🍥",
-            vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=26777821911:+26777821911\nitem1.X-ABLabel:Bot\nEND:VCARD`,
-          },
-        },
-      },
+    }
     }
   );
 }
