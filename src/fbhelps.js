a91d8519fe3409568162a469d1775020c5184c2d
Njabulo Jb <fanajb65@gmail.com>
2026-05-24 21:27:30 +0200

    Update fbhelps.js

--- src/fbhelps.js
+++ src/fbhelps.js
@@ -1,8 +1,84 @@
 const { fana } = require('../njabulo/fana');
-const fs = require('fs');
-const getFBInfo = require("@xaviabot/fb-downloader");
-const { default: axios } = require('axios');
-const config = require("../set");
+const { generateWAMessageFromContent, proto } = require('@whiskeysockets/baileys');
+
+// ---------- Help command ----------
+fana({
+  nomCom: "fbhelpi",
+  alias: ["facebookhelp", "fbguide"],
+  categorie: "Download",
+  reaction: "📚"
+}, async (dest, zk, commandeOptions) => {
+  const { ms } = commandeOptions;
+  
+  const interactiveMessage = generateWAMessageFromContent(dest, {
+    viewOnceMessage: {
+      message: {
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: `╭━━━━━━━━━━━━╮
+┃   📥 *FACEBOOK DOWNLOADER* 📥
+┣━━━━━━━━━━━━┫
+┃ 📌 *.facebook <link>* = HD
+┃ 📌 *.fb <link>* = SD
+┃ 📌 *.fbhelp* = This menu
+╰━━━━━━━━━━━━╯
+
+📝 *How to use:* Tap below, then paste your FB link`
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: '✨ Powered by NJABULO MD'
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: '📚 Facebook Help',
+            hasMediaAttachment: false
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+              {
+                name: "single_select",
+                buttonParamsJson: JSON.stringify({
+                  title: "📥 Choose Action",
+                  sections: [
+                    {
+                      title: "Download",
+                      rows: [
+                        {
+                          header: "HD Download",
+                          title: ".facebook <link>",
+                          description: "Download HD quality",
+                          id: ".facebook "
+                        },
+                        {
+                          header: "SD Download", 
+                          title: ".fb <link>",
+                          description: "Download SD quality",
+                          id: ".fb "
+                        }
+                      ]
+                    },
+                    {
+                      title: "Other",
+                      rows: [
+                        {
+                          header: "Help Menu",
+                          title: ".fbhelp",
+                          description: "Show menu again",
+                          id: ".fbhelp"
+                        }
+                      ]
+                    }
+                  ]
+                })
+              }
+            ]
+          })
+        })
+      }
+    }
+  }, { quoted: ms });
+
+  await zk.relayMessage(dest, interactiveMessage.message, { messageId: interactiveMessage.key.id });
+});
 
 // ---------- Help command ----------
 fana({
