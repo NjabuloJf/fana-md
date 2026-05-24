2addcaf219a2869b38f752797b504ae7f755f736
Njabulo Jb <fanajb65@gmail.com>
2026-05-24 13:10:58 +0200

    Update ping.js

--- src/ping.js
+++ src/ping.js
@@ -18,6 +18,10 @@ fana({
   try {
     const njabulox = [
       "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg.png",
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg2.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg3.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg4.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg5.png"
       "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg.png"
     ];
 
@@ -159,8 +163,13 @@ fana({
   const { repondre, ms } = commandeOptions;
   try {
     const njabulox = [
-      "https://files.catbox.moe/mh36c7.jpg",
-      "https://files.catbox.moe/bnb3vx.jpg"
+         "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg.png",
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg2.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg3.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg4.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg5.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg.png"
+
     ];
 
     const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
@@ -296,8 +305,13 @@ fana({
   const { repondre, ms } = commandeOptions;
   try {
     const njabulox = [
-      "https://files.catbox.moe/mh36c7.jpg",
-      "https://files.catbox.moe/bnb3vx.jpg"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg.png",
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg2.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg3.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg4.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg5.png"
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg.png"
+
     ];
 
     const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
