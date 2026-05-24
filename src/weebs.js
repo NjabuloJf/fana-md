080be9d37c9edfc055de592508791bfcd7066479
Njabulo Jb <fanajb65@gmail.com>
2026-05-25 00:11:47 +0200

    Update weebs.js

--- src/weebs.js
+++ src/weebs.js
@@ -5,11 +5,13 @@ const { generateWAMessageContent, generateWAMessageFromContent } = require('@whi
 
 // ── Random image for the header (used only for error messages) ─────
 const njabulox = [
-  "",
-  "https://files.catbox.moe/xjeyjh.jpg",
-  "https://files.catbox.moe/mh36c7.jpg",
-  "https://files.catbox.moe/u6v5ir.jpg",
-  "https://files.catbox.moe/bnb3vx.jpg",
+"https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg.png",
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg2.png",
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg3.png",
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg4.png",
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg5.png",
+      "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg.png",
+
 ];
 const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
 
